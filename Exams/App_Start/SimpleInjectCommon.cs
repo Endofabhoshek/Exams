3542cae5301126b857989d40e7d366d0197e4843
Abhihshek Pal <palabhishek322@gmail.com>
2020-01-27 02:32:36 +0530

    DI Sertup
    DOne

---
+++ Exams/App_Start/SimpleInjectCommon.cs
@@ -0,0 +1,15 @@
+﻿[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Exams.App_Start.SimpleInjectCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(Exams.App_Start.SimpleInjectCommon), "Stop")]
+
+namespace Exams.App_Start
+{
+
+    using Exams.IoC;
+    public class SimpleInjectCommon
+    {
+        public static void RegisterServices()
+        {
+            System.Web.Mvc.DependencyResolver.SetResolver(Injector.CreateContainer());
+        }
+    }
+}
